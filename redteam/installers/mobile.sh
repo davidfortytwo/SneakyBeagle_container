6799efd99d49f9bea5bcd752d04d83fb0b4995d9
cbk914 <cbk914@riseup.net>
2022-01-19 10:04:00 +0100

    Add mobile nuclei templates

--- redteam/installers/mobile.sh
+++ redteam/installers/mobile.sh
@@ -9,4 +9,5 @@ echo "Installing mobile tools"
 cd $WORKDIR
 git clone $GITHUB/MobSF/Mobile-Security-Framework-MobSF.git
 cd $WORKDIR
+git clone https://github.com/optiv/mobile-nuclei-templates
 echo "Done"
