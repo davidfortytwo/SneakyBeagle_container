dcaae92e098c4438182c0fcdcca5a4da679d6ede
cbk914 <cbk914@riseup.net>
2022-02-01 13:44:17 +0100

    Path fix

--- redteam/installers/mobile.sh
+++ redteam/installers/mobile.sh
@@ -13,33 +13,33 @@ apt update && apt install -y gradlew
 cd $WORKDIR
 git clone $GITHUB/MobSF/Mobile-Security-Framework-MobSF.git
 cd $WORKDIR
-git clone https://github.com/optiv/mobile-nuclei-templates
+git clone $GITHUB/mobile-nuclei-templates
 cd $WORKDIR
-git clone https://github.com/nettitude/scrounger.git && cd scrounger && pip install -r requirements.txt && python setup.py install
+git clone $GITHUB/nettitude/scrounger.git && cd scrounger && pip install -r requirements.txt && python setup.py install
 cd $WORKDIR
 git clone git@github.com:dstotijn/hetty.git && cd hetty && make build
 cd $WORKDIR
 # git clone https://github.com/frida/frida.git && cd frida && make
-git clone https://github.com/Nightbringer21/fridump.git
+git clone $GITHUB/Nightbringer21/fridump.git
 cd $WORKDIR
-git clone https://github.com/0xdea/frida-scripts.git
-git clone https://github.com/AloneMonkey/frida-ios-dump.git
+git clone $GITHUB/0xdea/frida-scripts.git
+git clone $GITHUB/AloneMonkey/frida-ios-dump.git
 cd $WORKDIR
-git clone https://github.com/dwisiswant0/apkleaks && cd apkleaks && pip3 install -r requirements.txt
+git clone $GITHUB/dwisiswant0/apkleaks && cd apkleaks && pip3 install -r requirements.txt
 cd $WORKDIR
-git clone https://github.com/FSecureLABS/drozer.git && cd drozer && make deb
+git clone $GITHUB/FSecureLABS/drozer.git && cd drozer && make deb
 cd $WORKDIR
-git clone https://github.com/iBotPeaches/Apktool.git && cd Apktool && ./gradlew build shadowJar
+git clone $GITHUB/iBotPeaches/Apktool.git && cd Apktool && ./gradlew build shadowJar
 cd $WORKDIR
-git clone https://github.com/b-mueller/apkx && cd apkx && ./install.sh
+git clone $GITHUB/b-mueller/apkx && cd apkx && ./install.sh
 cd $WORKDIR
-git clone https://github.com/pxb1988/dex2jar.git
-git clone https://github.com/Storyyeller/enjarify.git
-git clone https://github.com/skylot/jadx.git && cd jadx && ./gradlew dist
+git clone $GITHUB/pxb1988/dex2jar.git
+git clone $GITHUB/Storyyeller/enjarify.git
+git clone $GITHUB/skylot/jadx.git && cd jadx && ./gradlew dist
 cd $WORKDIR
-git clone https://github.com/java-decompiler/jd-gui.git && cd jd-gui && ./gradlew build
+git clone $GITHUB/java-decompiler/jd-gui.git && cd jd-gui && ./gradlew build
 cd $WORKDIR
-git clone https://github.com/linkedin/qark && cd qark && pip install -r requirements.txt && pip install . --user
+git clone $GITHUB/linkedin/qark && cd qark && pip install -r requirements.txt && pip install . --user
 cd $WORKDIR
 rm mobile.sh
 echo "Done"
